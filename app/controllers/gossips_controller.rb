1e9231d11478a90863accc5619bbe79722900206
apouille <nguon.melanie@gmail.com>
2019-02-07 18:43:46 +0100

    association user et potin

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -19,21 +19,13 @@ class GossipsController < ApplicationController
   end
 
   def create
-  	@gossip = Gossip.new(title: params[:title], content: params[:content], user_id: User.all.sample.id)
+  	@gossip = Gossip.new(title: params[:title], content: params[:content])
+    @gossip.user = current_user
     if @gossip.save
-      flash[:notice] = "Vous avez crée un potin avec succès"
+      flash[:notice] = "#{current_user.first_name} a crée un potin avec succès"
       redirect_to root_path
     else
       render "new"
-    end
-
-    @gossip = Gossip.create(params[:id])
-    @gossip.user = User.find_by(id: session[:user_id])
-    if @gossip.save
-      flash[:success] = "Potin bien créé !"
-      redirect_to root_path
-    else
-      render new
     end
 end
 
@@ -56,7 +48,7 @@ end
 
 
 		# if gossip.destroy
-      flash[:notice] = "Vous avez supprimé un potin avec succès"
+    flash[:notice] = "Vous avez supprimé un potin avec succès"
     # else
 		redirect_to root_path
 	end
